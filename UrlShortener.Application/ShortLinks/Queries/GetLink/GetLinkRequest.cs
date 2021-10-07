7670da29f34a0eeed17b6d34a557c7d9547d4dae
temicch <temicch@gmail.com>
2021-10-07 15:08:19 +0700

    feat: remove INotify
    Very inconvenient to service it

--- UrlShortener.Application/ShortLinks/Queries/GetLink/GetLinkRequest.cs
+++ UrlShortener.Application/ShortLinks/Queries/GetLink/GetLinkRequest.cs
@@ -1,13 +1,13 @@
 using System;
 using MediatR;
-using UrlShortener.Application.Interfaces;
 using UrlShortener.Domain.Common;
 using UrlShortener.Domain.Entities;
 using UrlShortener.Domain.Events;
 
 namespace UrlShortener.Application.Implementation.ShortLinks.Queries.GetLink
 {
-    public class GetLinkRequest : IRequest<ShortLink>, INotify<ShortLink>
+    //public class GetLinkRequest : IRequest<IResult<ShortLink>>
+    public class GetLinkRequest: IRequest<ShortLink>
     {
         public GetLinkRequest(string alias)
         {
